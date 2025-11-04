85f8c8e27b06636780bb1256d2f7d3cd3ee1a410
Muhammad Asyrafi <44029801+asyrafi-m@users.noreply.github.com>
2025-11-04 23:34:03 +0100

    Added copyright text.
    Added copyright text.

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,6 @@
   import MapView from "./Map.svelte";
 </script>
 
-<div style="width:100%;"><h1 style="text-align:center; margin: 1rem;">Historical Maps of Bataviaaaaaaaa</h1></div>
+<div style="width:100%;"><h1 style="text-align:center; margin: 1rem;">Historical Maps of Batavia</h1></div>
 
 <MapView />
